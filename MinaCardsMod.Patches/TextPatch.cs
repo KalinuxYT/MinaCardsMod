338649a8191b4a55e117a8955b922b01f63e644b
Kalinux <kalinuxburner@gmail.com>
2024-10-22 00:44:43 -0400

    Added the following: - Overwrites the discord URL

--- MinaCardsMod.Patches/TextPatch.cs
+++ MinaCardsMod.Patches/TextPatch.cs
@@ -21,126 +21,129 @@ namespace MinaCardsMod.Patches
             }
         }
     }
-
+    
     // Credits Pause
     
     [HarmonyPatch(typeof(PauseScreen), "OpenScreen")]
-    public class ChangeRoadmapPauseHeaderPatch
-    {
-        static void Postfix()
-        {
-            if (GameObject.Find("Title")?.GetComponent<TextMeshProUGUI>() is TextMeshProUGUI textComponent)
-            {
-                textComponent.text = "Special Thanks";
-            }
-            else
-            {
-                MinaCardsModPlugin.Log.LogWarning("ChangeRoadmapPauseHeaderPatch: Could not find 'RoadmapText' or its TextMeshProUGUI component. Probably a false positive due to bad, stupid, no good code, ignore if you just unpaused the game.");
-            }
-        }
-    }
-    [HarmonyPatch(typeof(PauseScreen), "OpenScreen")]
-    public class ChangeRoadmapPauseTextPatch
+    public class PauseScreenTextPatches
     {
         private static readonly string[] textValues = new string[]
         {
-            "", "Kali", "Vulgaris - Testing", "Samsa - Testing", "SCP - Misc. Feedback",
-            "Diamond - Artwork", "", "Minawan - Minasonas & Inspiration", "Iw3y - Audio", "",
-            "Starcat - Testing"
+            "", "Scaith - Artwork & Audio", "Samsa - Testing", "SCP - Misc. Feedback", "Minawan - Minasonas & Inspiration",
+            "Iw3y - Audio", "", "", "Starcat - Testing", "", "Vulgaris - Testing"
         };
         static void Postfix()
         {
+            var pauseScreen = CSingleton<PauseScreen>.Instance;
+            if (pauseScreen == null)
+            {
+                MinaCardsModPlugin.Log.LogWarning("PauseScreenTextPatches: PauseScreen instance not found.");
+                return;
+            }
+            if (pauseScreen.m_ScreenGrp != null && pauseScreen.m_ScreenGrp.activeSelf)
+            {
+                ApplyTextChanges();
+            }
+        }
+        private static void ApplyTextChanges()
+        {
+            TextPatchUtility.UpdateText("Title", "Special Thanks");
             for (int i = 1; i <= textValues.Length; i++)
             {
                 string targetName = $"Text ({i})";
-                GameObject targetObject = GameObject.Find(targetName);
-                if (targetObject == null)
-                {
-                    MinaCardsModPlugin.Log.LogWarning($"ChangeRoadmapPauseTextPatch: GameObject '{targetName}' not found in the scene. Probably a false positive due to bad, stupid, no good code, ignore if you just unpaused the game.");
-                    continue; 
-                }
-                TextMeshProUGUI textComponent = targetObject.GetComponent<TextMeshProUGUI>();
-                if (textComponent == null)
-                {
-                    MinaCardsModPlugin.Log.LogWarning($"ChangeRoadmapPauseTextPatch: TextMeshProUGUI component not found on GameObject '{targetName}'.");
-                    continue;
-                }
-                textComponent.text = textValues[i - 1];
+                TextPatchUtility.UpdateText(targetName, textValues[i - 1]);
             }
-            TextMeshProUGUI[] allTextComponents = GameObject.FindObjectsOfType<TextMeshProUGUI>();
-            if (allTextComponents.Length == 0)
-            {
-                MinaCardsModPlugin.Log.LogWarning("ChangeRoadmapPauseTextPatch: No TextMeshProUGUI components found in the scene.");
-                return;
-            }
-            foreach (var textComponent in allTextComponents)
-            {
-                if (textComponent.text == "-Customer review app")
-                {
-                    textComponent.text = "Scaith - Artwork & Audio";
-                }
-            }
+            TextPatchUtility.UpdateTextByContent("-Customer review app", "Diamond - Artwork");
+            TextPatchUtility.UpdateTextByContent("Scaith - Artwork & Audio", "Scaith - Artwork & Audio", removeStrikethrough: true);
         }
     }
-    
+
     // Credits Title
     
     [HarmonyPatch(typeof(TitleScreen), "Start")]
-    public class ChangeRoadmapTitleHeaderPatch
-    {
-        static void Postfix()
-        {
-            if (GameObject.Find("Title")?.GetComponent<TextMeshProUGUI>() is TextMeshProUGUI textComponent)
-            {
-                textComponent.text = "Special Thanks";
-            }
-            else
-            {
-                MinaCardsModPlugin.Log.LogWarning("ChangeRoadmapTitleHeaderPatch: Could not find 'RoadmapText' or its TextMeshProUGUI component.");
-            }
-        }
-    }
-    [HarmonyPatch(typeof(TitleScreen), "Start")]
-    public class ChangeRoadmapTitleTextPatch
+    public class TitleScreenTextPatches
     {
         private static readonly string[] textValues = new string[]
         {
-            "", "Kali", "Vulgaris - Testing", "Samsa - Testing", "SCP - Misc. Feedback",
-            "Diamond - Artwork", "", "Minawan - Minasonas & Inspiration", "Iw3y - Audio", "",
-            "Starcat - Testing"
+            "", "Scaith - Artwork & Audio", "Samsa - Testing", "SCP - Misc. Feedback", "Minawan - Minasonas & Inspiration",
+            "Iw3y - Audio", "", "", "Starcat - Testing", "", "Vulgaris - Testing"
         };
         static void Postfix()
         {
+            ApplyTextChanges();
+        }
+        private static void ApplyTextChanges()
+        {
+            TextPatchUtility.UpdateText("Title", "Special Thanks");
             for (int i = 1; i <= textValues.Length; i++)
             {
                 string targetName = $"Text ({i})";
-                GameObject targetObject = GameObject.Find(targetName);
-                if (targetObject == null)
-                {
-                    MinaCardsModPlugin.Log.LogWarning($"ChangeRoadmapTitleTextPatch: GameObject '{targetName}' not found in the scene.");
-                    continue;
-                }
-                TextMeshProUGUI textComponent = targetObject.GetComponent<TextMeshProUGUI>();
-                if (textComponent == null)
-                {
-                    MinaCardsModPlugin.Log.LogWarning($"ChangeRoadmapTitleTextPatch: TextMeshProUGUI component not found on GameObject '{targetName}'.");
-                    continue;
-                }
-                textComponent.text = textValues[i - 1];
+                TextPatchUtility.UpdateText(targetName, textValues[i - 1]);
             }
+            TextPatchUtility.UpdateTextByContent("-Customer review app", "Diamond - Artwork");
+            TextPatchUtility.UpdateTextByContent("Scaith - Artwork & Audio", "Scaith - Artwork & Audio", removeStrikethrough: true);
+        }
+    }
+
+    // For both Pause and Title text modification
+    
+    public static class TextPatchUtility
+    {
+        public static void UpdateText(string objectName, string newText)
+        {
+            if (GameObject.Find(objectName)?.GetComponent<TextMeshProUGUI>() is TextMeshProUGUI textComponent)
+            {
+                textComponent.text = newText;
+            }
+            else
+            {
+                MinaCardsModPlugin.Log.LogWarning($"TextPatchUtility: Could not find '{objectName}' or its TextMeshProUGUI component.");
+            }
+        }
+        public static void UpdateTextByContent(string originalText, string newText, bool removeStrikethrough = false)
+        {
             TextMeshProUGUI[] allTextComponents = GameObject.FindObjectsOfType<TextMeshProUGUI>();
             if (allTextComponents.Length == 0)
             {
-                MinaCardsModPlugin.Log.LogWarning("ChangeRoadmapTitleTextPatch: No TextMeshProUGUI components found in the scene.");
+                MinaCardsModPlugin.Log.LogWarning("TextPatchUtility: No TextMeshProUGUI components found in the scene.");
                 return;
             }
             foreach (var textComponent in allTextComponents)
             {
-                if (textComponent.text == "-Customer review app")
+                if (textComponent.text == originalText)
                 {
-                    textComponent.text = "Scaith - Artwork & Audio";
+                    textComponent.text = newText;
+                    if (removeStrikethrough && (textComponent.fontStyle & FontStyles.Strikethrough) == FontStyles.Strikethrough)
+                    {
+                        textComponent.fontStyle &= ~FontStyles.Strikethrough;
+                    }
                 }
             }
+        }
+    }
+    
+    // Replace Discord URL
+    
+    [HarmonyPatch(typeof(PauseScreen), "OnPressDiscordBtn")]
+    public class PauseScreenDiscordPatch
+    {
+        static bool Prefix()
+        {
+            Application.OpenURL("https://uwumarket.us/collections/cerbervt");
+            UnityAnalytic.JoinDiscord();
+            MinaCardsModPlugin.Log.LogInfo("PauseScreenDiscordPatch: Discord button URL modified on the pause screen.");
+            return false;
+        }
+    }
+    [HarmonyPatch(typeof(TitleScreen), "OnPressDiscordBtn")]
+    public class TitleScreenDiscordPatch
+    {
+        static bool Prefix()
+        {
+            Application.OpenURL("https://uwumarket.us/collections/cerbervt");
+            UnityAnalytic.JoinDiscord();
+            MinaCardsModPlugin.Log.LogInfo("TitleScreenDiscordPatch: Discord button URL modified on the title screen.");
+            return false;
         }
     }
 /*
