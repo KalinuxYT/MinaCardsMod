1848b722bdcc60a8d739410a4fdcca40493bb9d6
Kalinux <kalinuxburner@gmail.com>
2024-10-22 14:00:41 -0400

    Content update for release

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -10,6 +10,6 @@ using System.Security.Permissions;
 [assembly: Debuggable(DebuggableAttribute.DebuggingModes.Default | DebuggableAttribute.DebuggingModes.DisableOptimizations | DebuggableAttribute.DebuggingModes.IgnoreSymbolStoreSequencePoints | DebuggableAttribute.DebuggingModes.EnableEditAndContinue)]
 [assembly: AssemblyCompany("MinaCardsMod")]
 [assembly: AssemblyConfiguration("Debug")]
-[assembly: AssemblyFileVersion("1.4.3.272")]
+[assembly: AssemblyFileVersion("1.4.3.273")]
 [assembly: AssemblyProduct("MinaCardsMod")]
 [assembly: AssemblyTitle("MinaCardsMod")]
